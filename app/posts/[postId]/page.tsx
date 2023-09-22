27e697313baf9f9e4b6cbcc41fbf732fb9c12bab
Muhammad Sohaib <msohaib@ssasoft.com>
2023-09-22 17:29:51 +0500

    staic is off

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 import { notFound } from "next/navigation"
 
 
-export function generateStaticParams(){
-    const posts = getSortedPostsData();
+// export function generateStaticParams(){
+//     const posts = getSortedPostsData();
 
-    return posts.map((post)=>{
-        postId : post.id
-    })
-}
+//     return posts.map((post)=>{
+//         postId : post.id
+//     })
+// }
 
 export function generateMetadata({ params }: { params: { postId: string } }) {
     const posts = getSortedPostsData()
