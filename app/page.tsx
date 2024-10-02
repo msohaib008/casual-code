acb63baa5420afa81e48722e675f2496fb3b4ddb
Muhammad Sohaib <msohaib@ssasoft.com>
2024-10-02 13:25:31 +0500

    link added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Post from "./components/Post"
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+  const handleGoBack = () => {
+    router.back(); // This will take the user back to the previous page in the browser history
+  };
   return (
     <main className="px-6 mx-auto">
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome 🙌&nbsp;
         <span className="whitespace-nowrap">
-          I&apos;m <span className="font-bold">Sohaib</span>
+          I&apos;m <span className="font-bold" onClick={handleGoBack}>Sohaib</span>
         </span>
       </p>
       <Post />
